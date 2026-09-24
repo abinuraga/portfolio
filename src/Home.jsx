4c14483a9a0e61700499aaeb1bc46174811e19f1
unknown <LENOVO@PC-IT.cipconsul.local>
2026-09-24 16:42:23 +0700

    update portofolio

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,17 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 import profil from "./img/spider.jpg";
 
 function Home() {
+    
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+        });
+    }, []);
     return (
         <div className ="min-h-screen bg-white text-gray-800">
 
@@ -35,7 +46,7 @@ function Home() {
                 <div className="mx-auto grid max-w-6xl items-center gap-12 md:grid-cols-2">
 
                     {/* text */}
-                    <div className="text-center md:text-left">
+                    <div className="text-center md:text-left" data-aos="fade-down" data-aos-delay="300">
 
                         <p className="mb-3 text-lg font-medium text-blue-600">Halo, Nama saya </p>
                         <h1 className="mb-3 text-lg font-medium text-blue-600"> Adam Levine</h1>
@@ -53,7 +64,8 @@ function Home() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex justify-center">
+
+                    <div className="flex justify-center" data-aos="slide-left" data-aos-delay="300">
 
                         <img src={profil} alt="Profile" className="h-72 w-72 rounded-full object-cover shadow-xl md:h-96 md:w-96"/>
                     </div>
@@ -69,27 +81,24 @@ function Home() {
 
                         <div className="mb-12 text-center">
 
-                            <p className="font-medium text-blue-600">About Me</p>
+                            <p className="font-medium text-blue-600" data-aos="fade-down" data-aos-delay="100">About Me</p>
 
                         </div>
 
                         <div className="grid items-center gap-12 md:grip-cols-2">
 
-                            {/* foto */}
-                            <div className="flex justify-center">
-
-                                <img src={profil} alt={profil} className="h-64 w-64 rounded-full object-cover shadow-lg"/>
-                            </div>
+                            
 
                             {/* informasi */}
                             <div>
-                                <h3 className="text-2xl font-bold"> Adame Levine</h3>
-                                <p className="mt-4 mb-6 leading-relaxed text-gray-600">Saya adalah seorang web developer yang memiliki ketertarikan
+                                <h3 className="text-2xl font-bold" data-aos="fade-up" data-aos-delay="100"> Adame Levine</h3>
+                                <p className="mt-4 mb-6 leading-relaxed text-gray-600" data-aos="fade-up" data-aos-delay="100">
+                                    Saya adalah seorang web developer yang memiliki ketertarikan
                                     terhadap pengembangan desain web dari wireframe hingga Front End Desainer
                                 </p>
                                 
                                 {/* data pribadi */}
-                                <div classname="mt-8 space-y-4">
+                                <div classname="mt-8 space-y-4" data-aos="fade-up" data-aos-delay="300">
                                     <div className="flex border-b pb-3">
                                         <span className="w-32 font-semibold">No telp : </span>
                                         <span className="text-gray-600">081344827354 (whatsapp) </span>
@@ -106,6 +115,12 @@ function Home() {
                                     </div>
 
                                     <div className="flex border-b pb-3">
+                                        <span className="w-32 font-semibold">Pendidikan : </span>
+                                        <span className="text-gray-600"> Universitas Gunadarma (2024)</span>
+                                    </div>
+
+
+                                    <div className="flex border-b pb-3">
                                         <span className="w-32 font-semibold">Profesi : </span>
                                         <span className="text-gray-600"> IT dan Front End website developer.</span>
                                     </div>
@@ -114,6 +129,39 @@ function Home() {
                             </div>
                         </div>
                     </div>
+            </section>
+
+            <section
+            id="pengalaman"
+            className="px-6 py-24">
+                <div className="mx-auto max-w-6xl">
+                    <div className="mb-12 text-center" data-aos="fade-down" data-aos-delay="200">
+                        <p className="font-medium text-blue-600">
+                            Experience
+                        </p>
+                        <h2 className="mt-2 text-4xl font-bold">Pengalaman Saya</h2>
+                    </div>
+
+                    {/* detail pengalaman */}
+
+                    <div className="relative space-y-9">
+                        <div className="absolute left-0 top-0 h-full w-[2px] bg-red-600"></div>
+                        <div className="relative ml-7 border-r-7 border-purple-600 rounded-lg bg-gray-200 p-4 shadow-sm" data-aos="slide-right" data-aos-delay="100">
+
+                            <div className="absolute -left-[35px] top-0 h-4 w-4 rounded-full bg-blue-500"></div>
+
+                                    <h3 className="flex items-center justify-between text-2xl mb-6 font-semibold text-black">
+                                    <span className="font-mono">INFINITE LEARNING (Batam) </span> <span className="text-medium">Jul 2023 - Des 2023</span>
+                                    </h3>
+                                    
+                                <ul className="space-y-3 text-medium text-gray-800">
+                                    Mengikuti program kampus merdeka (MSIB) selama 6 bulan sebagai fullstack web development,
+                                    berpengalaman sebagai Project Manager dan Front End Developer
+                                </ul>
+                        </div>
+                                </div>
+
+                </div>
             </section>
 
             <section
